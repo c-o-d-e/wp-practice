755eea9875436b8d583c26274147a5e565b295b4
c-o-d-e <swapyuk@gmail.com>
2022-10-20 16:01:08 +0100

    loop for posts to the front-page

--- wp-content/themes/practice-template/front-page.php
+++ wp-content/themes/practice-template/front-page.php
@@ -4,7 +4,17 @@
     
     
 		<article class="content px-3 py-5 p-md-5">
-	   		
+	   	
+		<!-- Loop to query database to fetch post and content from pages in wp -->
+		<?php
+			if( have_posts()){
+				while( have_posts()){
+					the_post();
+					the_content();
+				}
+			}
+		?>
+
 	    </article>
 	    
     
